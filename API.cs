b0a17ffb2635930d23cbf6bcab2ae4dc069d4bb8
hamstar <webutek@gmail.com>
2017-11-14 02:46:05 -0800

    v1.1.3.1 * Minor internal refactoring

---
+++ API.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Terraria;
+
+
+namespace MotionBlurs {
+	public static class MotionBlursAPI {
+		public static MotionBlursConfigData GetModSettings() {
+			return MotionBlursMod.Instance.Config.Data;
+		}
+
+
+		public static void BeginCustomEntityBlur( Entity ent, Func<Entity, int> intensity_func ) {
+			if( ent is NPC ) {
+				var npc_info = MyNpcInfo.GetNpcInfo<MyNpcInfo>( ent.whoAmI );
+				if( npc_info == null ) { return; }
+
+				npc_info.Fx.SetCustomIntensity( intensity_func );
+			} else if( ent is Projectile ) {
+				var proj_info = MyProjectileInfo.GetProjInfo<MyProjectileInfo>( ent.whoAmI );
+				if( proj_info == null ) { return; }
+
+				proj_info.Fx.SetCustomIntensity( intensity_func );
+			} else {
+				throw new Exception( "Invalid entity type." );
+			}
+		}
+
+
+		public static void EndCustomEntityBlur( Entity ent ) {
+			if( ent is NPC ) {
+				var npc_info = MyNpcInfo.GetNpcInfo<MyNpcInfo>( ent.whoAmI );
+				if( npc_info == null ) { return; }
+
+				npc_info.Fx.SetCustomIntensity( null );
+			} else if( ent is Projectile ) {
+				var proj_info = MyProjectileInfo.GetProjInfo<MyProjectileInfo>( ent.whoAmI );
+				if( proj_info == null ) { return; }
+
+				proj_info.Fx.SetCustomIntensity( null );
+			} else {
+				throw new Exception( "Invalid entity type." );
+			}
+		}
+	}
+}
--- MotionBlursMod.cs
+++ MotionBlursMod.cs
@@ -7,49 +7,9 @@ using Terraria.ModLoader;
 
 
 namespace MotionBlurs {
-	public static class MotionBlursAPI {
-		public static MotionBlursConfigData GetModSettings() {
-			return MotionBlursMod.Instance.Config.Data;
-		}
-
-
-		public static void BeginCustomEntityBlur( Entity ent, Func<Entity, int> intensity_func ) {
-			if( ent is NPC ) {
-				var npc_info = MyNpcInfo.GetNpcInfo<MyNpcInfo>( ent.whoAmI );
-				if( npc_info == null ) { return; }
-
-				npc_info.Fx.SetCustomIntensity( intensity_func );
-			} else if( ent is Projectile ) {
-				var proj_info = MyProjectileInfo.GetProjInfo<MyProjectileInfo>( ent.whoAmI );
-				if( proj_info == null ) { return; }
-
-				proj_info.Fx.SetCustomIntensity( intensity_func );
-			} else {
-				throw new Exception("Invalid entity type.");
-			}
-		}
-
-
-		public static void EndCustomEntityBlur( Entity ent ) {
-			if( ent is NPC ) {
-				var npc_info = MyNpcInfo.GetNpcInfo<MyNpcInfo>( ent.whoAmI );
-				if( npc_info == null ) { return; }
-
-				npc_info.Fx.SetCustomIntensity( null );
-			} else if( ent is Projectile ) {
-				var proj_info = MyProjectileInfo.GetProjInfo<MyProjectileInfo>( ent.whoAmI );
-				if( proj_info == null ) { return; }
-
-				proj_info.Fx.SetCustomIntensity( null );
-			} else {
-				throw new Exception( "Invalid entity type." );
-			}
-		}
-	}
-
-
-
     class MotionBlursMod : Mod {
+		public static MotionBlursMod Instance { get; private set; }
+
 		public static string GithubUserName { get { return "hamstar0"; } }
 		public static string GithubProjectName { get { return "tml-motionblurs-mod"; } }
 
@@ -60,13 +20,9 @@ namespace MotionBlurs {
 			if( Main.netMode != 0 ) {
 				throw new Exception( "Cannot reload configs outside of single player." );
 			}
-			if( MotionBlursMod.Instance != null ) {
-				MotionBlursMod.Instance.Config.LoadFile();
-			}
+			MotionBlursMod.Instance.Config.LoadFile();
 		}
 		
-		public static MotionBlursMod Instance { get; private set; }
-
 		
 		////////////////
 
